fb7de0536660770ffb8affb3ce16ece58c351c1c
Shahariar Mahfuz <162718500+shahariarmahfuz@users.noreply.github.com>
2025-11-22 08:55:34 +0600

    Update worker.js

--- worker.js
+++ worker.js
@@ -80,7 +80,7 @@ async function sendTelegramMessage(token, chatId, text) {
 
 // --- জেমিনি ফাংশন ---
 async function askGemini(apiKey, history) {
-  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`;
+  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`;
   try {
     const response = await fetch(url, {
       method: "POST",
